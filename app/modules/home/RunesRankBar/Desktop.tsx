808628c232fcb4fcce7937bef52735bb7d08a401
pichaochang <pichaochang@gmail.com>
2025-02-26 16:41:30 +0800

    feat:update

--- app/modules/home/RunesRankBar/Desktop.tsx
+++ app/modules/home/RunesRankBar/Desktop.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useRef } from 'react';
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const data = Array.from({ length: 12 }, (_, i) => ({
   id: i + 1,
@@ -62,23 +68,33 @@ export default function CustomComponent() {
   };
 
   return (
-
     <div className=' gradient-border' >
       <div className='content-gb'>
         <div className="py-[20px] px-[30px]">
           <div className="flex justify-between items-center mb-4 ">
             <h2 className="text-white">CUMULATIVE AMOUNT</h2>
             <div className='flex items-center'>
-              <Image className='cursor-pointer' onClick={handlePrev} src="/images/icons/filter.png" alt="Previous" width={13} height={15} />
-              <Image className='cursor-pointer' onClick={handlePrev} src="/images/icons/arrow-bottom.png" alt="Previous" width={13} height={15} />
+              <Select>
+                <SelectTrigger className="bg-[#18141a] border-none">
+                  <SelectValue className='bg-[#18141a]'/>
+                </SelectTrigger>
+                <SelectContent className='bg-[#18141a]'>
+                  <SelectItem value="light">
+                    <Image className='cursor-pointer' src="/images/icons/filter.png" alt="Filter" width={13} height={15} />
+                  </SelectItem>
+                  <SelectItem value="dark">
+                    <Image className='cursor-pointer' src="/images/icons/filter.png" alt="Filter" width={13} height={15} />
+                  </SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
           <div className="flex items-end mb-4 overflow-x-auto cursor-pointer scrollbar-hidden" ref={scrollRef} style={{ maxWidth: 'calc(72px * 7 + 16px * 6)' }}>
             {data.map((item) => (
               <div key={item.id} id={`item-${item.id}`} className="flex flex-col items-center pr-4">
-                <div className='h-[130px] w-[72px] flex flex-col justify-end'>
+                <div className='h-[130px] w-[40px] xl:h-[130px] xl:w-[72px] flex flex-col justify-end'>
                   <div
-                    className={`w-[72px] cursor-pointer rounded-[10px] ${selected === item.id ? 'bg-gradient-to-b from-blue-500 to-purple-500' : 'bg-gray-700'
+                    className={`w-[40px] xl:h-[130px] xl:w-[72px] cursor-pointer rounded-[10px] ${selected === item.id ? 'bg-gradient-to-b from-blue-500 to-purple-500' : 'bg-gray-700'
                       }`}
                     style={{ height: `${Math.min(item.value / 10, 130)}px` }}
                     onClick={() => handleImageClick(item.id)}
@@ -92,7 +108,7 @@ export default function CustomComponent() {
                       <div className="text-white mt-2">{item.name}</div>
                     }
                   </div>
-                  <div className='sm:h-[70px] sm:w-[70px] h-[40px] w-[40px]'>
+                  <div className='xl:h-[70px] xl:w-[70px] h-[40px] w-[40px]'>
                     <Image onClick={() => handleImageClick(item.id)} src={item.image} alt={item.name} width={70} height={70} />
                   </div>
                 </div>
@@ -101,7 +117,7 @@ export default function CustomComponent() {
           </div>
         </div>
       </div>
-      <div className='w-full flex justify-between px-[20px] bottom-[58px] absolute z-20'>
+      <div className='w-full flex justify-between px-[20px] bottom-[70px] xl:bottom-[85px] absolute z-20'>
         <Image className='cursor-pointer' onClick={handlePrev} src="/images/icons/prev.png" alt="Previous" width={13} height={15} />
         <Image className='cursor-pointer' onClick={handleNext} src="/images/icons/next.png" alt="Next" width={13} height={15} />
       </div>
